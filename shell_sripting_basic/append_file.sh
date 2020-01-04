a869a670d2255562f8e59e8416c8cfb5f782914f
crimemastergogo-dev <satvik307@gmail.com>
2020-01-04 22:21:18 +0530

    append in file

--- shell_sripting_basic/append_file.sh
+++ shell_sripting_basic/append_file.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+#this tutorial show how to append some file using shell scripting
+
+echo -e "Enter the name of the file : \c"
+read file_name
+
+if [ -f $file_name ]
+then
+    if [ -w $file_name ]
+    then
+        echo "Type some text(ctr+d to quit)"
+        cat >> $file_name       #for appending we use >>.... if > is used file will be overwritten
+    else
+        echo "file don't have the wrtie permission"
+    fi
+else
+    echo "$file_name not exits"
+fi
+
