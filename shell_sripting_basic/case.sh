5372f553b019c20db4b9207a9a49be6ed77ec363
crimemastergogo-dev <satvik307@gmail.com>
2020-01-05 14:52:56 +0530

    final cases

--- shell_sripting_basic/case.sh
+++ shell_sripting_basic/case.sh
@@ -3,21 +3,30 @@
 #calculator using case
 
 echo -e "Enter num1 : \c"
-read $num1
+read num1
 echo -e "Enter num2 : \c"
-read $num2
-echo -e "Enter choice (+,-,*,/,) : \c"
-read $choice
+read num2
+echo "1.ADD"
+echo "2.SUBSTRACT"
+echo "3.MULTIPLY"
+echo "4.DIVIDE"
+echo -e "Enter choice  : \c"
+read choice
 
 case $choice in
-    "sum" )
-        echo "SUM = $num1+$num2 " | bc ;;
-    "sub" )
-        echo "SUB = $num1-$num2 " | bc;;
-    "mul" )
-        echo "MUL = $num1*$num2 " | bc;;
-    "div" )
-        echo "DIV = $num1/$num2 " | bc;;
+    1 )
+        res=`echo " $num1+$num2 " | bc`
+        ;;
+    2 )
+        res=`echo " $num1-$num2 " | bc`
+        ;;
+    3 )
+        res=`echo " $num1\*$num2 " | bc`
+        ;;
+    4 )
+        res=`echo "scale=2;$num1/$num2 " | bc`
+        ;;
     * )
         echo "WRONG CHOICE";;
 esac
+echo "output = $res"
