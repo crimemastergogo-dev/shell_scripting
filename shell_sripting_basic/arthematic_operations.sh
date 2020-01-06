650f6c25774d42658a174f3978485eb3227f0b8c
crimemastergogo-dev <satvik307@gmail.com>
2020-01-06 14:27:48 +0530

    if else calc

--- shell_sripting_basic/arthematic_operations.sh
+++ shell_sripting_basic/arthematic_operations.sh
@@ -2,21 +2,29 @@
 
 #basic calculator using if else and arthematic operations
 
-plus="+"
-sub="-"
-div="/"
-mul="*"
 echo -e "Enter num 1 : \c "
-read $num1
+read num1
 echo -e "Enter num 2 : \c "
-read $num2
-echo "Enter operation (+,-,*,/) :"
-read $choice
+read num2
+echo -e "Enter operation (+,-,*,/) : \c"
+read choice
 
-if [ "$choice" -eq "$plus" ]
+if [ "$choice" = "+" ]
 then
     echo -e "sum = : \c " 
     echo $(( num1 + num2 ))
+elif [ "$choice" = "-" ]
+then
+    echo -e "sub = : \c"
+    echo $(( num1-num2 ))
+elif [ "$choice" = "*" ]
+then
+    echo -e "mul = : \c"
+    echo "$num1*$num2" | bc
+elif [ "$choice" = "/" ]
+then
+    echo -e "div = : \c"
+    echo $(( num1/num2 ))
 else
     echo "Wrong choice"
 fi
