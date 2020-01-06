16ba42d18a35d7b262d559f0ae3b6ddc0929cff2
crimemastergogo-dev <satvik307@gmail.com>
2020-01-06 17:06:11 +0530

    reading file

--- shell_sripting_basic/while.sh
+++ shell_sripting_basic/while.sh
@@ -31,5 +31,28 @@ done
  #  sleep 1          #paue the loop for 1 sec
 #done
 
+#reading file uing while loop
+#there are several ways to read file using while loop
 
+#first method: INPUT Redirection
+while read file_name     
+do
+    echo $file_name
+done < test_file.txt #input redirection
+
+#second method: read file in single variables and then print it
+cat test_file.txt | while read file_name     
+do
+    echo $file_name
+done < test_file.txt #input redirection
+
+#third method :using IFS (internal field seprator ) used by bash to use internal word splitting 
+#i.e how to recognize word boundaries 
+
+while IFS= read -r file_name   #not assigning read to IFS we are assigning a space to IFS -r is used to prevent 
+                               #backslash,esc to interpreted
+#while IFS=' ' read -r file_name    
+do
+    echo $file_name
+done < test_file.txt #input redirection
 
