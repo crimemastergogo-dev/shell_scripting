13a9ee80045531f579b2f8dc40ecc8708ec2fb56
crimemastergogo-dev <satvik307@gmail.com>
2020-01-06 21:44:54 +0530

    for basic

---
+++ shell_sripting_basic/for_loop.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+#three formats of for loopd
+
+#***********FIRST***************#
+#for VARIABLE in 1 2 3 4 5 .. N
+#do
+#    command1
+#    command2
+#    commandN
+#done
+
+#**********SECOND**************#
+
+#for VARIABLE in file1 file2 file3
+#do
+#    command1 on $VARIABLE
+#    command2
+#    commandN
+#done
+
+#********THIRD*****************#
+
+#for OUTPUT in $(Linux-or-Unix-command)
+#do
+#    command1 on $OUTPUT
+#    command2 on $OUTPUT
+#    commandN
+#done
+
+#**********FOURTH**************#
+#for (( initialization; condition; itration ))
+#do
+#    command1
+#    command2
+#    command3
+#done
+
+for i in 1 2 3 4 5
+do
+    echo $i
+    sleep 1
+
+done
+
+for i in {1..10}
+do
+    echo $i
+    sleep 1
+
+done
+
+for i in {1..10..2} #{START..END..INCREMENT}
+do
+    echo $i
+    sleep 1
+
+done
+
+for (( i=0; i<5; i++ ))
+do
+    echo $i
+    sleep 1
+done
+
